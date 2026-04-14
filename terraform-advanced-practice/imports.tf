f10d1ec31310dbb701d6c9d9596c1552cb71e63a
Sandip Patil <96sandypatil@gmail.com>
2026-04-14 12:40:24 +0530

    varible reference added

---
+++ terraform-advanced-practice/imports.tf
@@ -0,0 +1,18 @@
+import {
+  id = "i-031519d3714431da5"
+  to = aws_instance.k8s-server
+}
+
+resource "aws_instance" "k8s-server" {
+  ami = "ami-0ec10929233384c7f"
+  instance_type = "t3.medium"
+  key_name = "k8s-server-key"
+  tags = {
+    Name = "k8s-server-prod"
+  }
+}
+
+resource "aws_ec2_instance_state" "instance_stae" {
+    state = "running"
+    instance_id = aws_instance.k8s-server.id
+}
--- terraform-advanced-practice/outputs.tf
+++ terraform-advanced-practice/outputs.tf
@@ -5,3 +5,8 @@ output "ec2_public_ip" {
 output "ec2_public_dns" {
   value = aws_instance.my-terra-ec2.public_dns
 }
+
+
+output "ec2_public_ip_k8s-server-prod" {
+  value = aws_instance.k8s-server.public_ip
+}
