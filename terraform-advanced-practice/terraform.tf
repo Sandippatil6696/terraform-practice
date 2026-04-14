f105d345a99811f27be29cb6a49a92de3dbd8439
Sandip Patil <96sandypatil@gmail.com>
2026-04-14 10:48:53 +0530

    remote backed apply to ec2 instance

--- terraform-advanced-practice/terraform.tf
+++ terraform-advanced-practice/terraform.tf
@@ -5,4 +5,11 @@ terraform {
       version = "6.40.0"
     }
   }
+
+  backend "s3" {
+    bucket = "rmt-backend-bucket-036815016260"
+    dynamodb_table = "rmt-backend-bucket-tbl"
+    key = "terraform.tfstate"
+    region = "us-east-1"
+  }
 }
