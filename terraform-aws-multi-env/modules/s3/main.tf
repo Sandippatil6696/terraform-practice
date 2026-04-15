a776ede1cd286cbec5aa83bbe0aa8b6edcf08f3d
Sandip Patil <96sandypatil@gmail.com>
2026-04-15 10:08:36 +0530

    multi env added

--- terraform-aws-multi-env/modules/s3/main.tf
+++ terraform-aws-multi-env/modules/s3/main.tf
@@ -1,9 +1,9 @@
 resource "aws_s3_bucket" "multi-env-s3-bucket" {
-  bucket = "${var.s3_bucket_name}-${count.index + 1 }"
+  bucket = "${var.env}-${var.s3_bucket_name}-${count.index+1}"
   count = var.s3_bucket_count
 
   tags = {
-    Name = "${var.s3_bucket_name}-${count.index + 1 }"
+    Name = "${var.env}-${var.s3_bucket_name}-${count.index+1}"
     Environment = var.env
   }
 }
